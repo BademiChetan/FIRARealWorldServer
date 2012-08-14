eb51cb2c88eae3773c995af2c2dafa932beada21
Chetan Bademi <chetanbademi@gmail.com>
2012-08-15 04:18:34 +0530

    Removed headers files from algo.cpp

--- algo.cpp
+++ algo.cpp
@@ -1,20 +1,3 @@
-#include <cv.h>
-#include <highgui.h>
-#include <ctime> 
-#include <boost/thread.hpp>
-#include "SerialStream.h"
-#include "select_arena.h"
-#include"process_image.h"
-#include "elec.h"
-#include "contours.h"
-#include "colors.h"
-#include "our_bot.h"
-#include "opp_bot.h"
-#include "ball.h"
-#include "update_frame.h"
-#include "global_var.h"
-#include "kick_off_calibration.h"
-#include "bot_actions.h"
 #include "algo.h" 
 using namespace cv;
 using namespace std;
